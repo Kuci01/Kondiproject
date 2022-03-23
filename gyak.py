2f435f190b10bc726409a5a91e8e1054607a5372
Kuci01 <mgeri2006@gmail.com>
2022-03-23 07:55:53 +0100

    Fájlok hozzá adása

---
+++ gyak.py
@@ -0,0 +1,9 @@
+keresztnev = input("Add meg a kereszt neved:")
+csaladnev = input ("Add meg a csalad neved is:")    
+print("Add meg a teljes neved: " + csaladnev + " " + keresztnev)
+a = input ("Mondj egy számot")
+b = input("Mondj még egy számot")
+if a > b : 
+    print (a) 
+else:
+    print(b)
